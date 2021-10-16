9c508135f68a7b7f2e5b13666f1696ba599370d6
elagutie <elagutie@bancolombia.com.co>
2021-10-16 07:23:16 -0500

    202110160722 - created shared component ( card-item, navbar )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CardItemComponent } from './shared/components/card-item/card-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardItemComponent
   ],
   imports: [
     BrowserModule,
